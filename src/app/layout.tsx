53d15f82dd7d49917a0ebd48812809a829221268
mosesmkrs <karisamoses392@gmail.com>
2024-02-28 19:53:52 +0300

    modes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-100`}>
+      <body className={`${inter.className} bg-slate-100 text-black`}>
         {children}
         <ToastContainer />
         </body>
